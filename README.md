02f681933fea60a6a1576606e8b647ad9865363a
Bode Fuwa <bodefuwa@gmail.com>
2016-03-21 15:09:48 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Lasvue Data
+
+This project contains data that powers lasvue. Lasvue is a resource site for the city of Lagos.
+
+### Contents
+* Geolocations
+* Websites and Links
+* State service resources
---
+++ seeds.rb
@@ -0,0 +1,63 @@
+# Lasvue Data
+# Strictly confidential
+# 2016 Rawneed, Inc.
+
+Division.delete_all
+LocalGovernmentArea.delete_all
+LocalCouncilDevelopment.delete_all
+HealthCenter.delete_all
+FireStation.delete_all
+EducationInstitution.delete_all
+TaxOffice.delete_all
+DriversLicenseCenter.delete_all
+PoliceCommand.delete_all
+
+Division.delete_all
+LocalGovernmentArea.delete_all
+LocalCouncilDevelopment.delete_all
+HealthCenter.delete_all
+# Lagos Island Maternity Hospital
+# General Hospital, Lagos
+# General Hospital, Gbagada
+# General Hospital, Orile Agege
+# General Hospital, Badagry
+# General Hospital, Isolo
+# General Hospital, Ikorodu
+# General Hospital, Surulere
+# General Hospital, Epe
+# General Hospital, Apapa
+# General Hospital, Alimosho
+# General Hospital, Mushin
+# General Hospital, Somolu
+# General Hospital, Ifako-Ijaiye
+# General Hospital, Ibeju-Lekki
+# General Hospital, Ajeromi
+# General Hospital, Agbowa
+# Mainland Hospital, Yaba
+# Health Centre, Onikan
+# Massey Children Hospital
+# Health Centre, Ebute-Metta
+# Health Centre, Harvey Road
+# Health Centre, Ijede
+# Medical Stores, Oshodi
+FireStation.delete_all
+EducationInstitution.delete_all
+TaxOffice.delete_all
+DriversLicenseCenter.delete_all
+# Bariga Driving Licence Office
+# Ikeja Driving Licence Office
+# Ikorodu Driving Licence Office
+# Ojodu Driving Licence Office
+# Lagos Island Diving Licence Office
+# Okokomaiko Driving License Office
+# Motor Vehicle Administration Agency HQ
+PoliceCommand.delete_all
+# State Command HQ
+# Area A Command
+# Area B Command
+# Area C Command
+# Area D Command
+# Area E Command
+# Area F Command
+# Area G Command
+# Area H Command
