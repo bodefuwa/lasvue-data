d3965737b24f6ab1e68085ee59f0c6925b3b6df7
Bode Fuwa <bodefuwa@gmail.com>
2016-03-21 15:32:01 -0400

    added skeleton

--- seeds.rb
+++ seeds.rb
@@ -1,7 +1,7 @@
 # Lasvue Data
 # Strictly confidential
 # 2016 Rawneed, Inc.
-
+# Drop all data before loading
 Division.delete_all
 LocalGovernmentArea.delete_all
 LocalCouncilDevelopment.delete_all
@@ -12,10 +12,19 @@ TaxOffice.delete_all
 DriversLicenseCenter.delete_all
 PoliceCommand.delete_all
 
-Division.delete_all
-LocalGovernmentArea.delete_all
-LocalCouncilDevelopment.delete_all
-HealthCenter.delete_all
+# load all data
+Division.create(
+  # {name: '', longtitude: '', latitude: ''},
+)
+LocalGovernmentArea.create(
+  # {name: '', longtitude: '', latitude: '', division: ''},
+)
+LocalCouncilDevelopment.create(
+  # {name: '', longtitude: '', latitude: '', division: ''},
+)
+HealthCenter.create(
+  # {name: '', longtitude: '', latitude: '', address: '', division: ''},
+)
 # Lagos Island Maternity Hospital
 # General Hospital, Lagos
 # General Hospital, Gbagada
@@ -40,10 +49,18 @@ HealthCenter.delete_all
 # Health Centre, Harvey Road
 # Health Centre, Ijede
 # Medical Stores, Oshodi
-FireStation.delete_all
-EducationInstitution.delete_all
-TaxOffice.delete_all
-DriversLicenseCenter.delete_all
+FireStation.create(
+  # {name: '', longtitude: '', latitude: '', address: '', division: ''},
+)
+EducationInstitution.create(
+  # {name: '', longtitude: '', latitude: '', address: '', division: ''},
+)
+TaxOffice.create(
+  # {name: '', longtitude: '', latitude: '', address: '', division: ''},
+)
+DriversLicenseCenter.create(
+  # {name: '', longtitude: '', latitude: '', address: '', division: ''},
+)
 # Bariga Driving Licence Office
 # Ikeja Driving Licence Office
 # Ikorodu Driving Licence Office
@@ -51,7 +68,9 @@ DriversLicenseCenter.delete_all
 # Lagos Island Diving Licence Office
 # Okokomaiko Driving License Office
 # Motor Vehicle Administration Agency HQ
-PoliceCommand.delete_all
+PoliceCommand.create(
+  # {name: '', longtitude: '', latitude: '', address: '', division: ''},
+)
 # State Command HQ
 # Area A Command
 # Area B Command
